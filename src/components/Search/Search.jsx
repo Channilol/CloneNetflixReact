d751b5b3aec6e34e07fddf37b9fbcb75b8ce6372
Francesco Cannizzo <fcannizzo97@gmail.com>
2023-12-22 17:48:46 +0100

    little fix mobile comments

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -39,7 +39,7 @@ class Search extends React.Component {
             <>
             <div className="searchPage">
                 <div className="closeSearch">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="rgba(255,255,255,0.8)" class="bi bi-x-circle-fill" viewBox="0 0 16 16" onClick={this.props.onClick}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="rgba(255,255,255,0.8)" className="bi bi-x-circle-fill" viewBox="0 0 16 16" onClick={this.props.onClick}>
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z"/>
                     </svg>
                 </div>
